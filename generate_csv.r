18a9e40e594b9ece4df6ffe0377de0e3fc6503ce
willilam <la.william4@gmail.com>
2022-02-21 17:28:13 +0000

    added csv files

---
+++ generate_csv.r
@@ -0,0 +1,16 @@
+generated_stims <- readRDS("~/colexification_experiment/generated_stims.RDS")
+
+
+firstTrial <- generated_stims[[1]]
+
+pairs = data.frame(firstTrial[5])
+
+
+
+x <- data.frame(pairs["pairs.pair1"], pairs["pairs.pair2"])
+stims <- firstTrial["stims"]
+words <- firstTrial["words"]
+
+write.csv(stims, file="~/colexification_experiment/stims.csv")
+write.csv(words, file="~/colexification_experiment/words.csv")
+write.csv(x, file="~/colexification_experiment/pairs.csv")
